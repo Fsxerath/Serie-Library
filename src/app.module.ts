a262e47eccdee8799120683e427127480246b548
Fsxerath <frediangarita1999@gmail.com>
2023-10-25 15:09:17 -0500

    Add Module of user, serie, and progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { SerieModule } from './serie/serie.module';
+import { ProgressModule } from './progress/progress.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, SerieModule, ProgressModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/serie/serie.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { SerieController } from './serie.controller';
+import { SerieService } from './serie.service';
+
+@Module({
+  controllers: [SerieController],
+  providers: [SerieService]
+})
+export class SerieModule {
+    
+}
