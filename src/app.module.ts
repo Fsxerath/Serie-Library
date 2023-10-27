03ea54431c3db3f28390ae0b4a2a014fbf6714b4
Fsxerath <frediangarita1999@gmail.com>
2023-10-27 17:38:17 -0500

    Add envoirment config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SeriesModule } from './series/series.module';
 import { ProgressModule } from './progress/progress.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UsersModule, SeriesModule, ProgressModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    UsersModule,
+    SeriesModule,
+    ProgressModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigService } from '@nestjs/config';
+import { CORS } from './constants';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors(CORS);
+
+  console.log(configService.get('HOLA'));
+
   await app.listen(3000);
 }
 bootstrap();
