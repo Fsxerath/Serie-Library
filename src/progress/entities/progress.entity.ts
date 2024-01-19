85fdfde3ac4da01176bfe9a9c6ba8b42c403dfad
Fsxeraht <frediangarita1999@gmail.com>
2024-01-19 15:03:15 -0500

    add decorator to pregress.entity

--- src/progress/entities/progress.entity.ts
+++ src/progress/entities/progress.entity.ts
@@ -1,8 +1,22 @@
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
 export class Progress {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   chapter: string;
+  @Column()
   season: string;
+  @Column()
   resume: string;
+  @CreateDateColumn()
+  dateCreated: string;
+  @UpdateDateColumn()
   dateUpdate: string;
   user_id: number;
 }
