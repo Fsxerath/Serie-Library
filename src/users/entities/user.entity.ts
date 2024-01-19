262786bf2f14c8d418e232e95025ef9586b61630
Fsxeraht <frediangarita1999@gmail.com>
2024-01-19 14:48:20 -0500

    add decorators to user.entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,12 @@
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column({ length: 100, unique: true })
   email: string;
+  @Column({ length: 50 })
   username: string;
+  @Column({ length: 256 })
   password: string;
 }
