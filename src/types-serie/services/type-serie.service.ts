723025e76223b0271968398d350bba6ca052fc8d
Fsxeraht <frediangarita1999@gmail.com>
2024-01-23 07:48:42 -0500

    create services for typeSerie

--- src/types-serie/services/type-serie.service.ts
+++ src/types-serie/services/type-serie.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Typeserie } from '../entities/typeSerie.entity';
+import { Repository } from 'typeorm';
+import { CreateTypeSerieDto } from '../dtos/createTypeSerie.dto';
 
 @Injectable()
-export class TypeSerieService {}
+export class TypeSerieService {
+  constructor(
+    @InjectRepository(Typeserie)
+    private readonly typeSerieRepository: Repository<Typeserie>,
+  ) {}
+
+  async getAllTypeSeries(): Promise<Typeserie[]> {
+    return await this.typeSerieRepository.find();
+  }
+
+  async createTypeSerie(series: CreateTypeSerieDto): Promise<Typeserie> {
+    return await this.typeSerieRepository.save(series);
+  }
+}
--- src/types-serie/types-serie.module.ts
+++ src/types-serie/types-serie.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeSerieController } from './controllers/type-serie.controller';
 import { TypeSerieService } from './services/type-serie.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Typeserie } from './entities/typeSerie.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Typeserie])],
   controllers: [TypeSerieController],
   providers: [TypeSerieService],
 })
