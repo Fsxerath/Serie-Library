6798a3aec73d35fb9bde95d4aee640481d362169
Fsxeraht <frediangarita1999@gmail.com>
2024-02-07 08:13:13 -0500

    finish the progress services

--- src/progress/services/progress.service.ts
+++ src/progress/services/progress.service.ts
@@ -11,10 +11,48 @@ export class ProgressService {
     @InjectRepository(Progress)
     private readonly progressRepository: Repository<Progress>,
   ) {}
-  async createProgress(createProgressDto: CreateProgressDto, user: User) {
+  async createProgress(
+    createProgressDto: CreateProgressDto,
+    user: User,
+  ): Promise<Progress> {
     return await this.progressRepository.save({
       ...createProgressDto,
       user,
     });
+  }
+  async findOneProgress(id: string, user: User): Promise<Progress> {
+    return await this.progressRepository.findOneOrFail({
+      where: {
+        id,
+        user: {
+          id: user.id,
+        },
+      },
+    });
+  }
+  async getProgressByUser(user: User): Promise<Progress[]> {
+    return await this.progressRepository.find({
+      where: {
+        user: {
+          id: user.id,
+        },
+      },
+    });
+  }
+  async updateProgress(
+    id: string,
+    progress: Partial<Progress>,
+  ): Promise<Progress> {
+    const searchProgress = await this.findOneProgress(id, progress.user);
+    const updatedProgress = Object.assign(searchProgress, progress);
+    const saveProgress = await this.progressRepository.save(updatedProgress);
+    if (!saveProgress) throw new Error('Error updating progress');
+    return saveProgress;
+  }
+  async deleteProgress(id: string, user: User): Promise<Progress> {
+    const progress = await this.findOneProgress(id, user);
+    const deleteProgress = await this.progressRepository.remove(progress);
+    if (!deleteProgress) throw new Error('Error deleting progress');
+    return deleteProgress;
   }
 }
