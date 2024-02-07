0a697ae87ab19f0342be0c8ce565d8987b37ecb2
Fsxeraht <frediangarita1999@gmail.com>
2024-02-07 08:30:09 -0500

    finish the progress controller

--- src/progress/controllers/progress.controller.ts
+++ src/progress/controllers/progress.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ProgressService } from '../services/progress.service';
+import { CreateProgressDto } from '../dtos/createProgress.dto';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { UpdateProgressDto } from '../dtos/updateProgress.dto';
 
 @Controller('progress')
-export class ProgressController {}
+export class ProgressController {
+  constructor(private readonly progresServices: ProgressService) {}
+  @Get()
+  getProgressByUser(@GetUser() user: User) {
+    return this.progresServices.getProgressByUser(user);
+  }
+  @Get('/:id')
+  getOneProgress(@Param('id') id: string, @GetUser() user: User) {
+    return this.progresServices.findOneProgress(id, user);
+  }
+  @Post()
+  createProgress(
+    @Body() createProgressDto: CreateProgressDto,
+    @GetUser() user: User,
+  ) {
+    return this.progresServices.createProgress(createProgressDto, user);
+  }
+  @Patch('/:id')
+  updateProgress(
+    @Param('id') id: string,
+    @Body() progress: Partial<UpdateProgressDto>,
+  ) {
+    return this.progresServices.updateProgress(id, progress);
+  }
+  @Delete('/:id')
+  deleteProgress(@Param('id') id: string, @GetUser() user: User) {
+    return this.progresServices.deleteProgress(id, user);
+  }
+}
