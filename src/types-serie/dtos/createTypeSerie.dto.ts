3dbeabb7f0c0a222d26e70e0dd9f6f89e9b05d87
Fsxerath <frediangarita1999@gmail.com>
2024-08-12 17:46:57 -0500

    add properties on typeSeries module

--- src/types-serie/dtos/createTypeSerie.dto.ts
+++ src/types-serie/dtos/createTypeSerie.dto.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateTypeSerieDto {
   @IsNotEmpty()
   @IsString()
   @MinLength(3)
+  @ApiProperty({
+    example: 'manga',
+    type: String,
+    minLength: 3,
+    required: true,
+  })
   type: string;
   @IsNotEmpty()
   @IsString()
   @MinLength(5)
+  @ApiProperty({
+    example: 'japanese comic',
+    type: String,
+    minLength: 5,
+    required: true,
+  })
   description: string;
 }
--- src/types-serie/dtos/updateTypeSerie.dto.ts
+++ src/types-serie/dtos/updateTypeSerie.dto.ts
@@ -1,12 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdateTypeSerieDto {
   @IsOptional()
   @IsString()
   @MinLength(3)
+  @ApiProperty({
+    example: 'manga',
+    type: String,
+    minLength: 3,
+  })
   type: string;
   @IsOptional()
   @IsString()
   @MinLength(5)
+  @ApiProperty({
+    example: 'medium used to express ideas with images',
+    type: String,
+    minLength: 5,
+  })
   description: string;
 }
