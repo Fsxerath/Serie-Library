071825b76c23f782ad94f813b6bca075304407dc
Fsxerath <frediangarita1999@gmail.com>
2024-08-13 16:01:49 -0500

    finish the API documentation

--- src/types-serie/controllers/type-serie.controller.ts
+++ src/types-serie/controllers/type-serie.controller.ts
@@ -11,11 +11,12 @@ import { TypeSerieService } from '../services/type-serie.service';
 import { CreateTypeSerieDto } from '../dtos/createTypeSerie.dto';
 import { UpdateTypeSerieDto } from '../dtos/updateTypeSerie.dto';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @Controller('typeSeries')
 @UseGuards(AuthGuard())
 @ApiTags('typeSeries')
+@ApiBearerAuth()
 export class TypeSerieController {
   constructor(private readonly typeSerieService: TypeSerieService) {}
   @Get()
